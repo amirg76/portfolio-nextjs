fc26cadbf4cd5e697c97f0d1739c8f5ea272a2ba
amirg76 <amirg76@gmail.com>
2023-09-13 19:02:24 +0300

    add portfolio&about&contact pages

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,8 +1,28 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            width={400}
+            height={250}
+            src="/apps.jpg"
+            alt=""
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
